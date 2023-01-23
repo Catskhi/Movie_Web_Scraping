cdf9d418b44abac61bc11af4ed07f196ee76bb57
Catskhi <catskhi.studios@gmail.com>
2023-01-23 15:39:19 -0300

    Adjusted to get by the requests lib instead of Selenium

--- background_scraping.py
+++ background_scraping.py
@@ -1,18 +1,7 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from time import sleep
+import requests 
 
 movie_list = ['Fight Club']
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'}
 
-option = Options()
-option.add_argument('--headless')
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=option)
-
-for movie in movie_list:
-    driver.get("https://www.google.com/")
-    print(driver.find_element('xpath', '/html/body/div[1]/div[1]/div/div/div/div[1]/div/div[1]/a').text)
+response = requests.get('https://www.imdb.com/', headers=headers)
+print(response)
